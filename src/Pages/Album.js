7215352869650d79c6995331573c963f5092a550
ikuol1 <damienssopolo@gmail.com>
2022-10-26 22:44:07 +0200

    26-10-2022

--- src/Pages/Album.js
+++ src/Pages/Album.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import { SearchBar, SideBar, Index, Presentation, TrackIn } from '../components';
-import img3 from '../assets/img3.svg';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 
 const Album = () => {
     let {i} = useParams();
-    let musics = useSelector((state) =>state.music);
-    console.log(musics);
+    let playlists = useSelector((state) =>state.playlist);
+    console.log(playlists);
   return (
     <div className='relative flex' 
         style={{
-            backgroundImage:`url(${musics[i-1].images.coverart})`,
+            backgroundImage:`url(${playlists[i-1].cover})`,
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center',
             backdropFilter:'blur(10px)',
-            opacity:'0.4',
             width: '100%',
         }}
     >
@@ -26,60 +24,18 @@ const Album = () => {
             <SearchBar />
             <div className='flex-1 flex flex-row items-center ml-[80px] mt-[50px]'>
                 <Presentation 
-                    imgUrl={ musics[i-1].images.coverart } 
-                    title={musics[i-1].title} 
-                    description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis'}
+                    imgUrl={ playlists[i-1].cover } 
+                    title={playlists[i-1].title} 
+                    description={playlists[i-1].info}
                     duration={'64 songs ~ 16 hrs+'} 
                     />
             </div>
             <div className='flex flex-col gap-[20px] mt-[60px]'>
                 <TrackIn
-                    imgUrl={ img3 }
-                    title={'Let me love you ~ Krisx'}
+                    imgUrl={ playlists.files[i].cover }
+                    title={playlists.files[i].title}
                     type={'Single'}
-                    duration={'4:17'}
-                />
-                <TrackIn
-                    imgUrl={ img3 }
-                    title={'Let me love you ~ Krisx'}
-                    type={'Single'}
-                    duration={'4:17'}
-                />
-                <TrackIn
-                    imgUrl={ img3 }
-                    title={'Let me love you ~ Krisx'}
-                    type={'Single'}
-                    duration={'4:17'}
-                />
-                <TrackIn
-                    imgUrl={ img3 }
-                    title={'Let me love you ~ Krisx'}
-                    type={'Single'}
-                    duration={'4:17'}
-                />
-                <TrackIn
-                    imgUrl={ img3 }
-                    title={'Let me love you ~ Krisx'}
-                    type={'Single'}
-                    duration={'4:17'}
-                />
-                <TrackIn
-                    imgUrl={ img3 }
-                    title={'Let me love you ~ Krisx'}
-                    type={'Single'}
-                    duration={'4:17'}
-                />
-                <TrackIn
-                    imgUrl={ img3 }
-                    title={'Let me love you ~ Krisx'}
-                    type={'Single'}
-                    duration={'4:17'}
-                />
-                <TrackIn
-                    imgUrl={ img3 }
-                    title={'Let me love you ~ Krisx'}
-                    type={'Single'}
-                    duration={'4:17'}
+                    duration={playlists.files[i].duration}
                 />
             </div>
         </div>
